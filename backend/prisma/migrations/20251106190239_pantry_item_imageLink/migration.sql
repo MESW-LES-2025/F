293c99e0a6b702768f0c10fd55c810030ec3018b
Leonardo Magalhães <131615964+leonardomagalhaes21@users.noreply.github.com>
2025-11-09 17:12:30 +0000

    cicd: fix backend deploy (#56)
    * feat: add lint workflow (#49)
    
    * feat: add lint
    
    * fix lint
    
    * fix lint
    
    * fix remove build
    
    * cicd: added frontend deploy pipelines
    
    * cicd: fix in frontend deploy pipelines
    
    * cicd: fix in pipelines
    
    * cicd: deploy backend
    
    * cicd: fix deploy backend
    
    * cicd: try to fix deploy backend
    
    * cicd: try to fix deploy backend
    
    * cicd: try to fix deploy backend
    
    * cicd: fix bakend deploy
    
    ---------
    
    Co-authored-by: Nathália Couto <79584703+nathaliaacouto@users.noreply.github.com>

--- backend/prisma/migrations/20251106190239_pantry_item_imageLink/migration.sql
+++ backend/prisma/migrations/20251106190239_pantry_item_imageLink/migration.sql
@@ -1,8 +1,20 @@
 -- AlterTable
 ALTER TABLE "PantryItem" ALTER COLUMN "imageLink" DROP NOT NULL;
 
--- DropEnum
-DROP TYPE "public"."Unit";
+-- DropEnum (safe for all schemas)
+DO $$
+DECLARE
+    r record;
+BEGIN
+    FOR r IN
+        SELECT n.nspname AS schema_name, t.typname AS type_name
+        FROM pg_type t
+        JOIN pg_namespace n ON t.typnamespace = n.oid
+        WHERE t.typname = 'unit'
+    LOOP
+        EXECUTE format('DROP TYPE IF EXISTS %I.%I', r.schema_name, r.type_name);
+    END LOOP;
+END$$;
 
 -- CreateIndex
 CREATE INDEX "PantryItem_createdByUser_idx" ON "PantryItem"("createdByUser");
