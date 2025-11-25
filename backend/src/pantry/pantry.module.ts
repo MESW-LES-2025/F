b78d49711bf4f95d83233cff0cdca60e502aaee9
Gonçalo Remelhe <goncaloremelhe67@hotmail.com>
2025-11-25 16:14:55 +0000

    feat: inline notifications for low stock items

--- backend/src/pantry/pantry.module.ts
+++ backend/src/pantry/pantry.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PantryService } from './pantry.service';
 import { PantryController } from './pantry.controller';
+import { NotificationsModule } from 'src/notifications/notifications.module';
 
 @Module({
+	imports: [NotificationsModule],
 	controllers: [PantryController],
 	providers: [PantryService],
 	exports: [PantryService],
