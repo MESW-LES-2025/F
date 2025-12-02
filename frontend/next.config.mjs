4186bfdd45b9875def64cec4510d91e7b8bdf32d
leonardomagalhaes21 <leonardomagalhaess21@gmail.com>
2025-12-02 00:54:37 +0000

    chore: try fix deploy

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -15,16 +15,19 @@ const nextConfig = {
     root: path.resolve(__dirname),
   },
   async rewrites() {
-    const apiUrl = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000/api/v1';
-    // Remove trailing slash if present to avoid double slashes
-    const cleanApiUrl = apiUrl.replace(/\/$/, '');
-    
-    return [
-      {
-        source: '/api/:path*',
-        destination: `${cleanApiUrl}/:path*`,
-      },
-    ];
+	if (process.env.VERCEL) {
+		const apiUrl = process.env.NEXT_PUBLIC_API_URL;
+		// Remove trailing slash if present to avoid double slashes
+		const cleanApiUrl = apiUrl.replace(/\/$/, '');
+		
+		return [
+		{
+			source: '/api/:path*',
+			destination: `${cleanApiUrl}/:path*`,
+		},
+		];
+	}
+	return [];
   },
 }
 
